550678b9ffcfa2cacaa0d01c4a80094041bf53e1
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-04-19 18:37:13 +0100

    Add tags for activemq and qp

--- jira.py
+++ jira.py
@@ -6,19 +6,37 @@ import json
 
 SERVICES = {
     "TAG:REDUCE": {
-        "name": "reduce.isis.cclrc.ac.uk",
+        "name": "Host reduce.isis.cclrc.ac.uk is not responding",
         "link": "https://reduce.isis.cclrc.ac.uk/kibana/app/uptime",
         "jira_field": {
             "id":
-            "ari:cloud:graph::service/b0a0787a-e104-460d-ad03-f976135c8a6a/27d089ca-a0fd-11eb-be7a-128b42819424"
+            "ari:cloud:graph::run/b0a0787a-e104-460d-ad03-f976135c8a6a/27d089ca-a0fd-11eb-be7a-128b42819424"
         }
     },
     "TAG:REDUCESTATIC": {
-        "name": "reduce.isis.cclrc.ac.uk static files",
+        "name":
+        "Host of reduce.isis.cclrc.ac.uk static files is not responding",
         "link": "https://reduce.isis.cclrc.ac.uk/kibana/app/uptime",
         "jira_field": {
             "id":
-            "ari:cloud:graph::service/b0a0787a-e104-460d-ad03-f976135c8a6a/53493872-a0fd-11eb-9efb-128b42819424"
+            "ari:cloud:graph::run/b0a0787a-e104-460d-ad03-f976135c8a6a/53493872-a0fd-11eb-9efb-128b42819424"
+        }
+    },
+    "TAG:ACTIVEMQSTALE": {
+        "name": "Stale messages in ActiveMQ",
+        "link": "https://reduce.isis.cclrc.ac.uk/kibana",
+        "jira_field": {
+            "id":
+            "ari:cloud:graph::service/b0a0787a-e104-460d-ad03-f976135c8a6a/63e707a4-a0fd-11eb-9118-128b42819424"
+        }
+    },
+    "TAG:QUEUEPROCESSORS": {
+        "name":
+        "Something gone wrong with queue processors (this isn't used yet)",
+        "link": "https://reduce.isis.cclrc.ac.uk/kibana",
+        "jira_field": {
+            "id":
+            "ari:cloud:graph::service/b0a0787a-e104-460d-ad03-f976135c8a6a/6a3d6a58-a0fd-11eb-be82-128b42819424"
         }
     },
     "OTHER": {
@@ -45,18 +63,17 @@ def send(message: str):
 
     print("GET", json.dumps(json.loads(response.text), sort_keys=True))
 
-    if "TAG:REDUCESTATIC" in message:
-        tag = "TAG:REDUCESTATIC"
-    elif "TAG:REDUCE" in message:
-        tag = "TAG:REDUCE"
-    else:
-        tag = "OTHER"
+    tag = "OTHER"
+    for key in SERVICES.keys():
+        if key in message:
+            tag = key
+            break
 
     payload = {
         "serviceDeskId": "1",
         "requestTypeId": "12",
         "requestFieldValues": {
-            "summary": f"Disruption of {SERVICES[tag]['name']}",
+            "summary": f"Disruption: {SERVICES[tag]['name']}",
             "description":
             f"A server log triggered this incident. Check {SERVICES[tag]['link']} for more info",
             "customfield_10036": [SERVICES[tag]['jira_field']]
